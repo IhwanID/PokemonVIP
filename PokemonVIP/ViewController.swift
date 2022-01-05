5a58dc1431912a342b2b9adea8a9bedc859da474
ihwan <ihwan.id@icloud.com>
2022-01-05 23:20:17 +0700

    Initial Commit

---
+++ PokemonVIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonVIP
+//
+//  Created by Ihwan on 05/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
